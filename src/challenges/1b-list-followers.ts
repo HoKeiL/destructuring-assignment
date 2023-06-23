be106ccd714792e160c241cb6c51bfc1ab6736e1
Your Name <you@example.com>
2023-06-23 14:13:11 +0100

    completed ex1-1b

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,7 +1,16 @@
+/**
+ * listFollowers lists by name the first two followers and announces how many others there are
+ * @param followerNames 
+ * @returns 
+ */
+
+
+
 function listFollowers(followerNames: string[]): string {
   // complete function
+  const  [first,second] = followerNames
   // use array destructuring, for practice!
-  return "fails test right now";
+  return `Followed by ${first}, ${second} and others`;
 }
 
 export default listFollowers;
